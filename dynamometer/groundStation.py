62a09643fd97bbc023b3be13d26963304a88b48c
MikoZ321 <0123456789miko@gmail.com>
2026-01-03 12:42:27 +0100

    add quit from menu

--- dynamometer/groundStation.py
+++ dynamometer/groundStation.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import QMainWindow, QApplication
 
 # TODO: display the readings in a GUI
 class Dashboard(QMainWindow):
+    '''Serves as the class container for the entire GUI'''
     def __init__(self, app) -> None:
         super().__init__()
         self.app = app
@@ -16,10 +17,15 @@ class Dashboard(QMainWindow):
         file_menu = menu_bar.addMenu("&File")
         file_menu.addAction("Save")
         file_menu.addAction("Save as ...")
-        file_menu.addAction("Quit")
+        quit_action = file_menu.addAction("Quit")
+        quit_action.triggered.connect(self.quit)
 
         settings_menu = menu_bar.addMenu("Settings")
         settings_menu.addAction("Select port")
+
+    def quit(self) -> None:
+        '''Method used to close the window and kill the application'''
+        self.app.quit()
 
 
 if __name__ == '__main__':
