a27c4760710206a46ae231c66ce4c2d505813cc1
MikoZ321 <0123456789miko@gmail.com>
2026-02-02 22:01:11 +0100

    create dummy mc code

--- dynamometer/dynamometer.ino
+++ dynamometer/dynamometer.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+// dummy code to test integration with dashboard on only one variable
 
+void setup() {
+  // arbitrarily set at highest possible, will be decreased after more calculations of data transfer needs
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // transmitting two alternating readings, delays correspond to expected sensor collection frequency of 100 Hz
+  Serial.println("10");
+  delay(10);
+  Serial.println("87");
+  delay(10);
 }
