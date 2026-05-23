17968eca32c109262e81527f05e8c91b3c7a1ed5
MikoZ321 <0123456789miko@gmail.com>
2026-05-23 12:40:15 +0200

    add telemetry frame structs

---
+++ dynamometer/onboard_computer/include/types.h
@@ -0,0 +1,57 @@
+// Contains all of the global type and enum definitions
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+typedef struct __attribute__((packed)) {
+   // Header
+   std::uint8_t sync_bytes[2]; // 0xAA 0x55
+   std::uint16_t sequence_number;
+   std::uint32_t timestamp_ms; 
+   // Tier A sensors
+   float acceleration_main[3];
+   float angular_velocity[3];
+   float acceleration_high_g[3];
+   float air_pressure;
+   float air_temperature;
+   float altitude;
+   float engine_thrust;
+   // Tier B sensors
+   float oxidizer_weight;
+   float fuel_pressure;
+   float oxidizer_pressure;
+   float pyro_battery_voltage;
+   float main_battery_voltage;
+   float engine_temperature_top;
+   float engine_temeprature_bottom;
+   std::uint8_t piston_position;
+   // Tier C sensors
+   float gps_latitude;
+   float gps_longitude;
+   float gps_altitude;
+   std::uint8_t gps_sattelite_count;
+   float main_battery_level;
+   // Tail metadata
+   std::uint16_t is_valid_reading_mask;
+   std::uint16_t crc;
+} full_telemetry_frame_t;
+
+typedef struct __attribute__((packed)) {
+   // Header
+   std::uint8_t sync_bytes[2]; // 0xBB 0x66
+   std::uint16_t sequence_number;
+   std::uint32_t timestamp_ms; 
+   // Tier A sensors
+   float acceleration_main[3];
+   float angular_velocity[3];
+   float acceleration_high_g[3];
+   float air_pressure;
+   float air_temperature;
+   float altitude;
+   float engine_thrust;
+   // Tail metadata
+   std::uint16_t is_valid_reading_mask;
+   std::uint16_t crc;
+} mini_telemetry_frame_t;
+#endif
